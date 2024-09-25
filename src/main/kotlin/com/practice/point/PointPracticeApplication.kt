4e37ef4b856a94d52c3fb085026c4f4c848c199c
임수신 <iss0416@naver.com>
2024-09-25 22:16:33 +0900

    포인트 연습 프로젝트 생성

---
+++ src/main/kotlin/com/practice/point/PointPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.practice.point
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PointPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<PointPracticeApplication>(*args)
+}
